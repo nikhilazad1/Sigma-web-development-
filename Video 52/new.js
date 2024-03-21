4fb918684ed6a6cf30c2bca97fe6b9a05923c6ee
nikhilazad1 <nikhilazad.18878@marwadieducation.edu.in>
2024-03-21 17:58:06 +0530

    Completed

--- Video 52/new.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello World")
-console.log("Code is running...")
-console.log("Code is also running...")
-console.log("Code is looking like a wow...")
--- Video 56/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-result1 = sum(3, 2)
-// result2 = sum(7, 5)
-// result3 = sum(3, 13, 1)
