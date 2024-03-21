d9538323ca7893f88978ce43a90cd4547dddc247
nikhilazad1 <nikhilazad.18878@marwadieducation.edu.in>
2024-03-21 17:34:57 +0530

    arrange this in serial number

---
+++ Video 55/index.js
@@ -0,0 +1,35 @@
+console.log("I am a tutorial on Loops")
+
+let a = 1;
+console.log(a)
+console.log(a+1)
+console.log(a+2)
+
+for (let i = 0; i < 100; i++) { 
+    console.log(a + i); 
+}
+
+
+let obj = {
+    name: "Harry",
+    role: "Programmer",
+    company: "CodeWithHarry AI"
+}
+
+for (const key in obj) {  
+        const Element=obj[key];
+        console.log(key,Element)
+}
+
+// let i = 0;
+// while (i<60000) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
+// because i is not less than 6
---
+++ Video 56/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+result1 = sum(3, 2)
+// result2 = sum(7, 5)
+// result3 = sum(3, 13, 1)
