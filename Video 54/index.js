114ad90e99b49ce2be7a208e548e4370d7356a91
nikhilazad1 <nikhilazad.18878@marwadieducation.edu.in>
2024-03-21 19:39:51 +0530

    Completed

---
+++ Video 54/index.js
@@ -0,0 +1,47 @@
+console.log("Hello World")
+
+let age = 1;
+// let grace = 2;
+
+// age += grace
+console.log(age)
+console.log(age + grace)
+console.log(age - grace)
+console.log(age * grace)
+console.log(age / grace)
+console.log(age ** grace)
+console.log(age % grace)
+
+if (age == 18){
+    console.log("you can drive")
+}
+
+else if (age == 0) {
+    console.log("Are you kidding?")
+}
+
+else if (age == 1) {
+    console.log("Are you again kidding?")
+}
+
+else {
+    console.log("You cannot drive");
+}
+
+a = 6;
+b = 8;
+let c = a > b ? (a - b) : (b - a);
+
+if(a>b) {
+    
+    let c = a - b;
+}
+else {
+    let c = a - b;
+}
+
+
+
+
+
+
