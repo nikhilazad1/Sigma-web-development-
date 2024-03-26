9c4f436f4d438e0942b3a2e40b96bd2c80f5f31d
nikhilazad1 <nikhilazad.18878@marwadieducation.edu.in>
2024-03-27 00:32:13 +0530

    completed

---
+++ Video 60/index.js
@@ -0,0 +1,27 @@
+let arr = [1, 2, 4, 5, 7]
+// index = 0, 1, 2, 3, 4
+// arr[0] = 5566;
+console.log(arr);
+console.log(arr[0])     
+console.log(arr[1])     
+console.log(arr[2])
+console.log(arr.length)
+console.log(arr, typeof arr); 
+
+// console.log(arr.tostring())
+console.log(arr.join(" and "))
+
+a = [1, 2, 3, 4, 5,]
+a.pop()
+
+let a1 = [1, 2, 3, 4]
+let a2 = [5, 6, 7, 8]
+let a3 = [9, 10, 11, 12]
+
+a1.concat(a2, a3) 
+
+let numbers = [1, 2, 3, 4, 5] 
+numbers.splice(1, 2)    
+numbers.splice(1, 3)  
+numbers.splice(1, 3, 222, 333) 
+// (4) [1, 222, 333, 5]
