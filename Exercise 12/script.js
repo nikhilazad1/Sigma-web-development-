1d435630bd355c64385f67d3ed1f7447b4b6977a
nikhilazad1 <nikhilazad.18878@marwadieducation.edu.in>
2024-06-18 00:27:33 +0530

    completed

---
+++ Exercise 12/script.js
@@ -0,0 +1,18 @@
+console.log("script.js.initializing")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+let boxes = document.querySelector(".container").children
+
+function getRandomcolor(){
+         let val1 = Math.ceil(0 + Math.random()* 255);
+         let val2 = Math.ceil(0 + Math.random()* 255);
+         let val3 = Math.ceil(0 + Math.random()* 255);    
+         return `rgb(${val1}, ${val2}, ${val3}`
+}
+
+Array.from(boxes).forEach(e=>{
+    e.style.backgroundColor = getRandomcolor()
+    e.style.color = getRandomcolor()
+})
+
---
+++ Video 51/README.md
@@ -0,0 +1,2 @@
+# Netflix-Clone
+Code for design netflix clone
